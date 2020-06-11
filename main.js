da7b21163dc2873ed7274d166b663a6f8d36e9f6
Joshua B Gutierrez <joshgm15@gmail.com>
2020-06-10 21:26:01 -0500

    Added functionality for menu section

--- main.js
+++ main.js
@@ -30,6 +30,10 @@ const slideHeader = document.querySelector('.slider-section h1');
 const slideSeparator = document.querySelector('.slider-section img');
 const slideDescription = document.querySelector('.slider-section p');
 const slideButton = document.querySelector('.slider-section button');
+const menuButtons = document.querySelectorAll('.menu-button');
+const menuItems = document.querySelectorAll('.menu-items');
+const menuButtonSelected = document.querySelector('.active');
+const selected = document.querySelector('.selected');
 
 // Global variables
 let slideIndex = 1;
@@ -79,9 +83,64 @@ function slideshowFadeIn() {
 		.from(slideButton, { opacity: 0 });
 }
 
+function initialButtonSelected() {
+	const initialButton = menuButtonSelected.getBoundingClientRect();
+
+	const initialCoords = {
+		width: initialButton.width,
+		height: initialButton.height,
+		left: initialButton.left + window.scrollX,
+		top: initialButton.top + window.scrollY
+	};
+
+	const { width, height, left, top } = initialCoords;
+
+	selected.style.width = `${width}px`;
+	selected.style.height = `${height}px`;
+	selected.style.left = `${left}px`;
+	selected.style.top = `${top}px`;
+}
+
+function displayMenuItemsSelected(e) {
+	e.preventDefault();
+	const menuItemSelected = document.querySelector(`#${this.dataset.target}`);
+	const menuButtonSelected = e.target;
+	const menuButtonSelectedCoords = menuButtonSelected.getBoundingClientRect();
+
+	const selectedCoords = {
+		width: menuButtonSelectedCoords.width,
+		height: menuButtonSelectedCoords.height,
+		left: menuButtonSelectedCoords.left + window.scrollX,
+		top: menuButtonSelectedCoords.top + window.scrollY
+	};
+
+	const { width, height, left, top } = selectedCoords;
+
+	selected.style.width = `${width}px`;
+	selected.style.height = `${height}px`;
+	selected.style.left = `${left}px`;
+
+	menuItems.forEach((menuItem) => {
+		menuItem.classList.remove('isVisible');
+		menuItemSelected.classList.add('isVisible');
+	});
+
+	menuButtons.forEach((button) => {
+		button.classList.remove('active');
+		menuButtonSelected.classList.add('active');
+	});
+}
+
 // Event Listeners
 sliderArrows.forEach((arrow) => {
 	arrow.addEventListener('click', (e) => {
 		play(e.target.classList);
 	});
 });
+
+menuButtons.forEach((button) => {
+	button.addEventListener('click', displayMenuItemsSelected);
+});
+
+initialButtonSelected();
+window.addEventListener('resize', initialButtonSelected);
