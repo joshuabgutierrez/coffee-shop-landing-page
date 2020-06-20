fe259cd6d3b9e29b7759a4fd34f001fa4a3fc4d2
Joshua B Gutierrez <joshgm15@gmail.com>
2020-06-19 21:38:36 -0500

    Data for slideshow

---
+++ sliderData.js
@@ -0,0 +1,23 @@
+const sliderData = [
+	{
+		id: 1,
+		header: 'Welcome to Savannah Coffee',
+		image: './images/showcase-image-1.jpg',
+		description:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae corrupti ut facere aperiam distinctio similique quisquam ad'
+	},
+	{
+		id: 2,
+		header: 'A brand new coffe shop',
+		image: './images/showcase-image-2.jpg',
+		description:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae corrupti ut facere aperiam distinctio similique quisquam ad'
+	},
+	{
+		id: 3,
+		header: 'Taste our best coffee',
+		image: './images/showcase-image-3.jpg',
+		description:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae corrupti ut facere aperiam distinctio similique quisquam ad'
+	}
+];
